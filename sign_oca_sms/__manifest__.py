be7bb3fb1cd7b2ff8c2c1be9535787d5e8ee97ac
BernatPForgeFlow <bernat.puig@forgeflow.com>
2024-02-29 10:23:11 +0100

    [FIX] sign_oca_sms: Rename module to 'Sign Oca Sms'

--- sign_oca_sms/__manifest__.py
+++ sign_oca_sms/__manifest__.py
@@ -1,9 +1,9 @@
 # Copyright 2023 ForgeFlow S.L. (http://www.forgeflow.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Kencove Sign",
+    "name": "Sign Oca Sms",
     "version": "16.0.1.0.0",
-    "category": "Sales/Sign",
+    'category': 'Sales/Sign',
     "summary": "Kencove Sign Customizations",
     "author": "ForgeFlow",
     "website": "https://gitlab.com/kencove/odoo/addons/ken",
