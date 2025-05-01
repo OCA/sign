15cc682565402d6bd0b0b02f56907b9acfaf9584
kobros-tech <alkobroslymohamed@gmail.com>
2025-05-01 19:29:05 +0300

    [IMP] survey_sign_oca: add end heading

--- survey_sign_oca/__manifest__.py
+++ survey_sign_oca/__manifest__.py
@@ -12,8 +12,6 @@
         "views/survey_user_views.xml",
         "views/res_config_settings_view.xml",
         "views/sign_oca_request_views.xml",
-        "views/survey_survey_views.xml",
-        "views/survey_templates.xml",
         "data/sign_oca_role.xml",
     ],
     "installable": True,
--- survey_sign_oca/models/__init__.py
+++ survey_sign_oca/models/__init__.py
@@ -2,4 +2,3 @@ from . import survey_invite
 from . import res_company
 from . import res_config_settings
 from . import sign_oca_request
-from . import survey_survey
