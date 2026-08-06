82c1cda1fff3117943fcd3b4b6b6c692b62dd546
Enric Tobella <enric.tobella@dixmit.com>
2026-08-06 15:40:20 +0200

    [FIX] sign_oca: Use odoo pdf libraries

--- sign_oca/models/sign_oca_request.py
+++ sign_oca/models/sign_oca_request.py
@@ -7,7 +7,6 @@ from base64 import b64decode, b64encode
 from hashlib import sha256
 from io import BytesIO
 
-from PyPDF2 import PdfFileReader, PdfFileWriter
 from reportlab.graphics.shapes import Drawing, Line, Rect
 from reportlab.lib.colors import black, transparent
 from reportlab.lib.styles import ParagraphStyle
@@ -18,6 +17,7 @@ from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 from odoo.http import request
 from odoo.tools import float_repr
+from odoo.tools.pdf import PdfFileReader, PdfFileWriter
 
 
 class SignOcaRequest(models.Model):
